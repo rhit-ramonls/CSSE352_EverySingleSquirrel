a9287af04ad152e5b9e45ae4ce83ce9b7b2ca5f8
Logan Ramon <ramonls@rose-hulman.edu>
2023-05-15 11:48:56 -0400

    Added main player movement and the three main scenes

---
+++ CSSE352_EverySingleSquirrel/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 4.5f;
+
+    private CircleCollider2D circle;
+    private Rigidbody2D body;
+
+    [SerializeField] public bool allowVerticalMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        circle = GetComponent<CircleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        Vector2 movement;
+        if (allowVerticalMovement)
+        {
+            float deltaY = Input.GetAxis("Vertical") * speed;
+            movement = new Vector2(deltaX, deltaY);
+            body.velocity = movement;
+        }
+        else
+        {
+            body.velocity = new Vector2(deltaX * speed, 0f);
+        }
+        
+    }
+}
